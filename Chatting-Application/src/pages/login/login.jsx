53dd47122e1e4dd3e707c6e9c4219543f8286961
mpxupd <srm27mohit@gmail.com>
2024-12-29 09:47:36 +0530

    Login & Profile Done

--- Chatting-Application/src/pages/login/login.jsx
+++ Chatting-Application/src/pages/login/login.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "./Login.css";
+import { useState } from "react";
 import assets from "../../assets/assets";
-import logo_big from "../../assets/assets";
+import logo_big from "../../assets/logo_big.png";
 
 const Login = () => {
+  const [current, setCurrent] = useState("Sign Up");
   return (
     <div className="login">
-      <img src={assets.logo_big} alt="" className="logo" />
+      <img src={assets.logo_big} alt="" className="logo_login" />
       <form className="login_form">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          className="Form-input"
-          required
-        />
+        <h2>{current}</h2>
+        {current == "Sign Up" ? (
+          <input
+            type="text"
+            placeholder="Username"
+            className="Form-input"
+            required
+          />
+        ) : null}
         <input
           type="email"
           placeholder="E-Mail Address"
@@ -27,15 +31,26 @@ const Login = () => {
           className="Form-input"
           required
         />
-        <button className="button" type="submit">
-          Sign Up
+        <button className="button_login" type="submit">
+          {current == "Sign Up" ? "Sign Up" : "Login Now"}
         </button>
         <div className="login-term">
           <input type="checkbox" />
           <p>Agree to the terms & conditions</p>
         </div>
         <div className="login-toggle">
-          Already have an account? <span>Click here!</span>
+          {current == "Sign Up"
+            ? "Already have an account? "
+            : "Create an Account? "}
+          <span
+            onClick={() => {
+              current == "Sign Up"
+                ? setCurrent("Login")
+                : setCurrent("Sign Up");
+            }}
+          >
+            Click here!
+          </span>
         </div>
       </form>
     </div>
--- Chatting-Application/src/pages/profile_update.jsx/profile_update.jsx
+++ Chatting-Application/src/pages/profile_update.jsx/profile_update.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import "./profile_update.css";
+import assets from "../../assets/assets";
 const profile_update = () => {
-  return <div></div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form className="form_profile">
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar" className="avatar">
+            <input type="file" id="avatar" accept=".png,.jpg,.jpeg" hidden />
+            <img src={assets.avatar_icon} alt="" className="Profile_upload" />
+            Uplad profile picture
+          </label>
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Your Name"
+              required
+              className="input_profile"
+            />
+            <textarea
+              placeholder="Write profile bio"
+              required
+              className="textarea_profile"
+            ></textarea>
+            <button className="button_profile">Update</button>
+          </div>
+        </form>
+        <img src={assets.logo_icon} alt="" className="logo_profile" />
+      </div>
+    </div>
+  );
 };
 
 export default profile_update;
